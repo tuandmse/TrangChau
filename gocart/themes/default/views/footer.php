9e00fa3284f1f64c71b0e41dc47c1bcef16dfa21
thanhtam4692 <xitrum4692@gmail.com>
2014-05-13 02:20:25 +0700

    Fix svg display

--- gocart/themes/default/views/footer.php
+++ gocart/themes/default/views/footer.php
@@ -1,6 +1,6 @@
     <footer class="footer">
 
-        <div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/drivenByGoCart.svg');?>" alt="Driven By GoCart"></a></div>
+        <div style="text-align:center;"><a href="http://gocartdv.com" target="_blank"><img src="<?php echo base_url('assets/img/login-logo.png');?>" alt="Driven By GoCart"></a></div>
         
     </footer>
 </div>
