a6a84373e2c3b46e8840762fbd68412ffdf5ac33
cuonghapvn <cuonghapvn@gmail.com>
2014-05-17 12:34:59 +0700

    Mail config

--- fashionshop/controllers/admin/orders.php
+++ fashionshop/controllers/admin/orders.php
@@ -216,7 +216,15 @@ class Orders extends Admin_Controller {
 			// send the message
    		$this->load->library('email');
 		
-		$config['mailtype'] = 'html';
+		$config = array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_port' => 465,
+            'smtp_user' => 'troioidathoi.com',
+            'smtp_pass' => 'matkhauvietthuong',
+            'mailtype' => 'html',
+            'charset' => 'utf-8'
+        );
 		
 		$this->email->initialize($config);
 
