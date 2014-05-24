62f08957e73965ac9c4cbd7dbc9322508d25f090
Nguyen Dang Thanh <thanhndse90043@fpt.edu.vn>
2014-05-24 07:26:51 +0700

    Việt hóa admin_lang, banner_lang, boxes_lang

--- fashionshop/language/vietnamese/admin_lang.php
+++ fashionshop/language/vietnamese/admin_lang.php
@@ -4,20 +4,20 @@ US English
 Admin Administrator Management Language
 ******************************************/
 
-$lang['admins']					= 'Administrators';
-$lang['admin_form']				= 'Administrator Form';
-$lang['add_new_admin']			= 'Add New Admin';
-$lang['error_self_delete']		= 'You cannot delete yourself';
-$lang['admin_not_found']		= 'The requested administrator could not be found';
-$lang['firstname']				= 'First Name';
-$lang['lastname']				= 'Last Name';
-$lang['email']                  = 'Email';
-$lang['username']               = 'Username';
-$lang['access']					= 'Access';
-$lang['password']				= 'Password';
-$lang['confirm_password']		= 'Confirm Password';
-$lang['message_user_saved']		= 'The administrator has been saved!';
-$lang['message_user_deleted']	= 'The administrator has been deleted.';
-$lang['error_username_taken']	= 'The requested username is already in use.';
-$lang['information']			= 'Information';
-$lang['confirm_delete']			= 'Are you sure you want to delete this administrator?';
+$lang['admins']					= 'Người quản trị' //'Administrators';
+$lang['admin_form']				= 'Mẫu quản trị' //'Administrator Form';
+$lang['add_new_admin']			= 'Thêm mới người quản trị' //'Add New Admin';
+$lang['error_self_delete']		= 'Bạn không thể tự xóa mình' //'You cannot delete yourself';
+$lang['admin_not_found']		= 'Không thể tìm thấy người quản trị được yêu cầu' //'The requested administrator could not be found';
+$lang['firstname']				= 'Họ' //'First Name';
+$lang['lastname']				= 'Tên' //'Last Name';
+$lang['email']                  = 'Email' //'Email';
+$lang['username']               = 'Tên đăng nhập' //'Username';
+$lang['access']					= 'Quyền đăng nhập' //'Access';
+$lang['password']				= 'Mật khẩu' //'Password';
+$lang['confirm_password']		= 'Xác nhận mật khẩu' //'Confirm Password';
+$lang['message_user_saved']		= 'Người quản trị đã được lưu' //'The administrator has been saved!';
+$lang['message_user_deleted']	= 'Người quản trị đã bị xóa' //'The administrator has been deleted.';
+$lang['error_username_taken']	= 'Tên đăng nhập đã được sử dụng' //'The requested username is already in use.';
+$lang['information']			= 'Thông tin' //'Information';
+$lang['confirm_delete']			= 'Bạn có chắc là mình muốn xóa người quản trị này?' //'Are you sure you want to delete this administrator?';
--- fashionshop/language/vietnamese/boxes_lang.php
+++ fashionshop/language/vietnamese/boxes_lang.php
@@ -4,21 +4,21 @@ US English
 Admin Boxes Language
 ******************************************/
 
-$lang['boxes']				= 'Boxes';
-$lang['box_info']			= 'Box Information';
-$lang['title']				= 'Title';
-$lang['link']				= 'Link';
-$lang['enable_on']			= 'Enable On';
-$lang['disable_on']			= 'Disable On';
-$lang['new_window']			= 'New Window';
-$lang['image']				= 'Image';
-$lang['current_file']		= 'Current File';
-$lang['confirm_delete_box']	= 'Are you sure you want to delete this box?';
-$lang['add_new_box']		= 'Add New Box';
-$lang['no_boxes']			= 'There are currently no boxes';
-$lang['message_box_saved']	= 'The box has been saved!';
-$lang['date_error']			= 'The "Disable On" date cannot come on or before the "Enable On" date.';
-$lang['box_form']			= 'Box Form';
-$lang['message_delete_box']	= 'The box has been deleted.';
-$lang['sort']				= 'Sort';
-$lang['sort_boxes']			= 'Boxes are sortable! Just drag and drop them in the order you would like for them to appear.';
+$lang['boxes']				= 'Các hộp' //'Boxes';
+$lang['box_info']			= 'Hộp thông tin' //'Box Information';
+$lang['title']				= 'Tiêu đề' //'Title';
+$lang['link']				= 'Liên kết' //'Link';
+$lang['enable_on']			= 'Bật Hiện' //'Enable On';
+$lang['disable_on']			= 'Bật Ẩn' //'Disable On';
+$lang['new_window']			= 'Cửa sổ mới' //'New Window';
+$lang['image']				= 'Hình ảnh' //'Image';
+$lang['current_file']		= 'Tập tin hiện tại' //'Current File';
+$lang['confirm_delete_box']	= 'Bạn có chắc là mình muốn xóa hộp này không?' //'Are you sure you want to delete this box?';
+$lang['add_new_box']		= 'Thêm hộp mới' //'Add New Box';
+$lang['no_boxes']			= 'Hiện tại không có hộp nào' //'There are currently no boxes';
+$lang['message_box_saved']	= 'Hộp đã được lưu lại!' //'The box has been saved!';
+$lang['date_error']			= 'Ngày "Bật Ẩn" không thể cùng hoặc sớm hơn ngày "Bật Hiện".' //'The "Disable On" date cannot come on or before the "Enable On" date.';
+$lang['box_form']			= 'Biểu mẫu hộp' //'Box Form';
+$lang['message_delete_box']	= 'Hộp đã được xóa.' //'The box has been deleted.';
+$lang['sort']				= 'Sắp xếp' //'Sort';
+$lang['sort_boxes']			= 'Hộp có thể sắp xếp được! Chỉ cần kéo thả theo trật tự bạn muốn.' //'Boxes are sortable! Just drag and drop them in the order you would like for them to appear.';
